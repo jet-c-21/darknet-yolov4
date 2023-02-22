d32c45172d649e415cf3f415f3419ba3ed20afa6
Jet (Ta-Wei Chien)$PUFF-X <67193495+jet-c-21@users.noreply.github.com>
2023-02-22 13:46:27 +0800

    Update jetson_nano_darknet_video.py

--- jetson_nano_darknet_video.py
+++ jetson_nano_darknet_video.py
@@ -110,6 +110,7 @@ def video_capture(frame_queue, darknet_image_queue):
         ret, frame = cap.read()
         if not ret:
             break
+
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame_resized = cv2.resize(frame_rgb, (darknet_width, darknet_height),
                                    interpolation=cv2.INTER_LINEAR)
@@ -117,6 +118,10 @@ def video_capture(frame_queue, darknet_image_queue):
         img_for_detect = darknet.make_image(darknet_width, darknet_height, 3)
         darknet.copy_image_from_bytes(img_for_detect, frame_resized.tobytes())
         darknet_image_queue.put(img_for_detect)
+
+        cv2.imshow('Video Capture', frame)
+        intput_key = cv2.waitKey(0)
+
     cap.release()
 
 
@@ -182,5 +187,5 @@ if __name__ == '__main__':
     print(f"Video Capture Info: src = {input_path}, w = {video_width}, h = {video_height}")
 
     Thread(target=video_capture, args=(frame_queue, darknet_image_queue)).start()
-    Thread(target=inference, args=(darknet_image_queue, detections_queue, fps_queue)).start()
-    Thread(target=drawing, args=(frame_queue, detections_queue, fps_queue)).start()
+    # Thread(target=inference, args=(darknet_image_queue, detections_queue, fps_queue)).start()
+    # Thread(target=drawing, args=(frame_queue, detections_queue, fps_queue)).start()
