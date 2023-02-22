ee86fa272c42ea2ce6bec881d2f1919344525bec
Jet (Ta-Wei Chien)$PUFF-X <67193495+jet-c-21@users.noreply.github.com>
2023-02-22 14:35:36 +0800

    tt

--- jetson_nano_darknet_video.py
+++ jetson_nano_darknet_video.py
@@ -118,11 +118,12 @@ def video_capture(frame_queue, darknet_image_queue):
         frame_resized = cv2.resize(frame_rgb, (darknet_width, darknet_height),
                                    interpolation=cv2.INTER_LINEAR)
 
+        frame_queue.put(frame)
+
         cv2.imshow('Preview', frame_resized)
         if cv2.waitKey(1) == ord('q'):
             break
-        
-        # frame_queue.put(frame)
+
         # img_for_detect = darknet.make_image(darknet_width, darknet_height, 3)
         # darknet.copy_image_from_bytes(img_for_detect, frame_resized.tobytes())
         # darknet_image_queue.put(img_for_detect)
